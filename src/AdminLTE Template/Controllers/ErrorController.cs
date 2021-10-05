0e310f6a557089c29cb11aeab44bc0acebd17c52
Snyder9195 <erikgone@gmail.com>
2021-10-05 10:28:56 -0400

    Created new solution and moved over proper csproj to correct sln

---
+++ src/AdminLTE Template/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE_Template.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        public IActionResult InternalServerError() => View();
+        
+        public IActionResult NotFound => View();
+    }
+}
---
+++ src/AdminLTE Template/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE_Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
